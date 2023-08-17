f527a6574cf00580d8ff53dce242c2e2a8348caa
Guilhrib <ribeiro.guilhermedesouza@gmail.com>
2023-08-16 22:20:20 -0300

    chore: add jest config for mongodb

---
+++ jest-mongodb-config.js
@@ -0,0 +1,12 @@
+module.export = {
+  mongodbMemoryServerOptions: {
+    instance: {
+      dbName: 'jest'
+    },
+    binary: {
+      version: '4.0.3',
+      skipMD5: true
+    },
+    autoStart: false
+  }
+}
