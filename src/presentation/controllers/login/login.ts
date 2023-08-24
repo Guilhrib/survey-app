861378841fbea58710acce5ed3705b430884ca09
Guilhrib <ribeiro.guilhermedesouza@gmail.com>
2023-08-24 15:30:24 -0300

    feat: login controller implementation

---
+++ src/presentation/controllers/login/login.ts
@@ -0,0 +1,42 @@
+import { InvalidParamError, MissingParamError } from '../../errors'
+import { badRequest, ok, serverError, unauthorized } from '../../helpers/http-helper'
+import {
+  Controller,
+  EmailValidator,
+  HttpRequest,
+  HttpResponse,
+  Authentication
+} from './login-protocols'
+
+export class LoginController implements Controller {
+  constructor (
+    private readonly emailValidator: EmailValidator,
+    private readonly authentication: Authentication
+  ) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const requiredFields = ['email', 'password']
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
+      }
+
+      const { email, password } = httpRequest.body
+      const isValid = this.emailValidator.isValid(email)
+      if (!isValid) {
+        return badRequest(new InvalidParamError('email'))
+      }
+
+      const accessToken = await this.authentication.auth(email, password)
+      if (!accessToken) {
+        return unauthorized()
+      }
+
+      return ok({ accessToken })
+    } catch (error) {
+      return serverError(error)
+    }
+  }
+}
