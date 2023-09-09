eeffeee9a94ebf9cb5f8ee1b07457acc928bd58b
Guilhrib <ribeiro.guilhermedesouza@gmail.com>
2023-09-09 15:03:21 -0300

    refactor: improve file names

--- src/main/factories/login/login-validation-factory.ts
+++ src/main/factories/login/login-validation-factory.ts
@@ -1,5 +1,5 @@
 import { Validation } from '../../../presentation/protocols/validation'
-import { EmailValidatorAdapter } from '../../../utils/email-validator-adapter'
+import { EmailValidatorAdapter } from '../../adapters/validator/email-validator-adapter'
 import {
   EmailValidation,
   RequiredFieldValidation,
