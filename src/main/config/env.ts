9e86a317c7621d6caae63f905b32073a55ace15f
Guilhrib <ribeiro.guilhermedesouza@gmail.com>
2023-09-09 15:26:05 -0300

    feat: add login factory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: global.__MONGO_URI__ || 'mongodb://0.0.0.0:27017/survey-app',
-  port: process.env.PORT ?? 5050
+  port: process.env.PORT ?? 5050,
+  jwtSecret: process.env.JWT_SECRET ?? 'ngfnj9872345++ohafg7$'
 }
