03bc7c2ec6763dcee0aa355c28eacc57dcd1081b
Guilhrib <ribeiro.guilhermedesouza@gmail.com>
2023-09-25 19:19:21 -0300

    chore: add docker compose

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,4 @@
 FROM node:18
 WORKDIR /usr/src/app
 COPY package*.json ./
-RUN npm install
-COPY ./dist ./dist
-EXPOSE 3000
-CMD ["npm", "start"]
+RUN npm install --only=production
--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: global.__MONGO_URI__ || 'mongodb://0.0.0.0:27017/survey-app',
+  mongoUrl: global.__MONGO_URI__ || 'mongodb://mongo/27017/survey-app',
   port: process.env.PORT ?? 5050,
   jwtSecret: process.env.JWT_SECRET ?? 'ngfnj9872345++ohafg7$'
 }
